3456b9f12a8324355cf5afcec523320c330428d5
valachi <impulse221@yandex.ru>
2011-11-29 21:08:23 +0400

    deleted files with tilde

--- lib/tasks/tilde.rb
+++
@@ -1,9 +0,0 @@
-desc 'Deletes all files that end with tilde (~)'
-task 'tilde' do
-files = []
-Dir.glob('**/*~').each do |file|
-File.delete(file)
-files << file
-end
-puts "Deleted the following files: #{files.join(', ')}"
-end
